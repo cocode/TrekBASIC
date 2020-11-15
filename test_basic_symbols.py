faf717561d866f2b96881b430108a2966f72471f
fridaynightwaltz <fnw.github.com@worldware.com>
2020-11-15 14:06:02 -0800

    Added more tests.

--- test_basic_symbols.py
+++ test_basic_symbols.py
@@ -9,6 +9,17 @@ class TestSymbolTable(TestCase):
         sym.put_symbol("A", 3, "variable", arg=None)
         value = sym.get_symbol("A")
         self.assertEqual(3, value)
+        sym_type = sym.get_symbol_type("A")
+        self.assertEqual("variable", sym_type)
+        self.assertTrue(sym.is_symbol_defined("A"))
+
+        sym.put_symbol("B", "ABC", "variable", arg=None)
+        sym_type = sym.get_symbol_type("B")
+        self.assertEqual("variable", sym_type)
+        self.assertTrue(sym.is_symbol_defined("B"))
+        # Check A still works.
+        sym_type = sym.get_symbol_type("A")
+        self.assertEqual("variable", sym_type)
         self.assertTrue(sym.is_symbol_defined("A"))
 
 
