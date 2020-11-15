3f4ba092607fa38e93aeae162b9c55ed6b8f64cc
fridaynightwaltz <fnw.github.com@worldware.com>
2020-11-15 14:33:46 -0800

    Added support for nested scopes, and test.

--- basic_symbols.py
+++ basic_symbols.py
@@ -24,6 +24,15 @@ class SymbolTable:
         new_table._symbols = self._symbols.copy()
         return new_table
 
+    def get_new_scope(self):
+        """
+        Gets a new scope for a user defined function.
+        This current SymbolTable (self) will be the outer scope for the new function.
+
+        :return: A new symbol table, that points to the current symbols table as an enclosing scope.
+        """
+        return SymbolTable(self)
+
     def get_active_symbols(self):
         """
         This returns the original, and changing it will affect program execution
@@ -34,16 +43,19 @@ class SymbolTable:
     def put_symbol(self, symbol:str, value, symbol_type:str, arg:str):
         self._symbols[symbol] = ste(value, symbol_type, arg)
 
+    def _is_local(self, symbol):
+        return symbol in self._symbols
+
     def is_symbol_defined(self, symbol:str):
         """
         :param symbol:
         :return:
         """
-        defined = symbol in self._symbols
-        if defined:
+        if self._is_local(symbol):
             return True
-        if self._enclosing_scope:
+        if self._enclosing_scope is not None:
             return self._enclosing_scope.is_symbol_defined(symbol)
+        return False
 
     def get_symbol(self, symbol:str):
         """
@@ -51,22 +63,30 @@ class SymbolTable:
         :param symbol:
         :return:
         """
-        assert_syntax(symbol in self._symbols, F"Variable {symbol} does not exist.")
-        return self._symbols[symbol].value
+        assert_syntax(self.is_symbol_defined(symbol), F"Variable {symbol} does not exist.")
+        if self._is_local(symbol):
+            return self._symbols[symbol].value
+        return self._enclosing_scope.get_symbol(symbol)
 
     def get_symbol_type(self, symbol:str):
         """
         :param symbol:
         :return:
         """
-        return self._symbols[symbol].type
+        assert_syntax(self.is_symbol_defined(symbol), F"Variable {symbol} does not exist.")
+        if self._is_local(symbol):
+            return self._symbols[symbol].type
+        return self._enclosing_scope.get_symbol_type(symbol)
 
     def get_symbol_arg(self, symbol:str):
         """
         :param symbol:
         :return:
         """
-        return self._symbols[symbol].arg
+        assert_syntax(self.is_symbol_defined(symbol), F"Variable {symbol} does not exist.")
+        if self._is_local(symbol):
+            return self._symbols[symbol].arg
+        return self._enclosing_scope.get_symbol_arg(symbol)
 
     def dump(self):
         pprint.pprint(self._symbols)
--- test_basic_symbols.py
+++ test_basic_symbols.py
@@ -12,14 +12,46 @@ class TestSymbolTable(TestCase):
         sym_type = sym.get_symbol_type("A")
         self.assertEqual("variable", sym_type)
         self.assertTrue(sym.is_symbol_defined("A"))
+        self.assertFalse(sym.is_symbol_defined("B"))
 
         sym.put_symbol("B", "ABC", "variable", arg=None)
-        sym_type = sym.get_symbol_type("B")
-        self.assertEqual("variable", sym_type)
+        self.assertEqual("variable", sym.get_symbol_type("B"))
+        self.assertEqual(None, sym.get_symbol_arg("B"))
         self.assertTrue(sym.is_symbol_defined("B"))
+
         # Check A still works.
         sym_type = sym.get_symbol_type("A")
         self.assertEqual("variable", sym_type)
         self.assertTrue(sym.is_symbol_defined("A"))
+
+    def test_scope(self):
+        outer = SymbolTable()
+        inner = outer.get_new_scope()
+        outer.put_symbol("A", 3, "variable", arg=None)
+        outer.put_symbol("B", 27, "variable", arg=None)
+
+        inner.put_symbol("A", "5", "function", arg="X")
+
+        # Check the symbol is there on the inner, and it shadows the outer scope.
+        self.assertEqual("5", inner.get_symbol("A"))
+        self.assertEqual("function", inner.get_symbol_type("A"))
+        self.assertEqual("X", inner.get_symbol_arg("A"))
+        self.assertTrue(inner.is_symbol_defined("A"))
+
+        # Check we can get symbols from outer via inner
+        self.assertEqual(27, inner.get_symbol("B"))
+        self.assertEqual("variable", inner.get_symbol_type("B"))
+        self.assertEqual(None, inner.get_symbol_arg("B"))
+        self.assertTrue(inner.is_symbol_defined("B"))
+
+        # Check outer scope still works.
+        self.assertEqual(3, outer.get_symbol("A"))
+        self.assertEqual("variable", outer.get_symbol_type("A"))
+        self.assertEqual(None, outer.get_symbol_arg("A"))
+        self.assertTrue(outer.is_symbol_defined("A"))
+
+
+
+
 
 
