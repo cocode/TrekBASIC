b0ded598df27f7aa32f109dfbb051a774e43d4b4
fridaynightwaltz <fnw.github.com@worldware.com>
2020-11-15 20:26:59 -0800

    I want to be able to run, at least, both versions of star trek that I have. This file with control dialect versions.

---
+++ basic_dialect.py
@@ -0,0 +1,8 @@
+"""
+Switches for controlling various language features that vary between dialects of basic
+"""
+
+# Other common option is **. Currently the lexer would require a code change for that.
+EXPONENTIATION_OPERATOR: str='^'
+# Change to 1 for 1-based arrays, 0 for 0-based.
+ARRAY_OFFSET: int=1
