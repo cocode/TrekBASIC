1c67c04dec47852f5bff7c079017c6d4912832f1
fridaynightwaltz <fnw.github.com@worldware.com>
2020-11-17 15:18:02 -0800

    Basic test for basic_statements.

---
+++ test_basic_statements.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+import basic_statements
+
+class Test(TestCase):
+    def test_statements(self):
+        # We don't have unit tests for stmt_xx functions. They get tested as part of the language
+        # but some unit tests would be good.
+        # Having this module will at least force the loading of basic_statements, and
+        # catch some errors.
+        pass
+
