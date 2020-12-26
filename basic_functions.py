745daf8b7f9a8d25c22142982f71e9a43f0b352b
cocode <cocode@github.com>
2020-12-26 15:02:55 -0800

    Missed adding this in the commit before last.

---
+++ basic_functions.py
@@ -0,0 +1,37 @@
+"""
+Trying to get all the information about functions in one place, to make it easier to add new ones.
+Not working yet.
+"""
+import random
+from math import sin, cos, sqrt, log
+
+import basic_operators as bo
+
+
+class PredefinedFunctions:
+    def __init__(self):
+        spaces = bo.StrMonoOp(lambda x: " " * int(x), return_type="str")
+                # To add another internal function.
+                # 1. Add it here.
+                # 2. Add it to basic_lexer.BUILT_IN_FUNCTIONS
+                # 3. Add it to the list of internal functions in basic_interpreter.Executor.run_program
+        self.functions = {
+            "ABS":    bo.MonoOp(lambda x: abs(x)),
+            "ASC":    bo.StrMonoOp(lambda x: ord(x), return_type="num"),
+            "CHR$":   bo.StrMonoOp(lambda x: chr(int(x), return_type="str")),
+            "COS":    bo.MonoOp(lambda x: cos(x)),
+            "INT":    bo.MonoOp(lambda x: int(x)),
+            "LEFT$":  bo.StrOp(lambda x: x[0][:int(x[1])], "LEFT$", 2),
+            "LEN":    bo.StrOp(lambda x: len(x), "LEN", 1, "num"),  # This needs to return an int, unlike the other str functions.
+            "LOG":    bo.MonoOp(lambda x: log(x)),
+            "MID$":   bo.StrOp(lambda x: x[0][int(x[1]) - 1:int(x[1]) - 1 + int(x[2])], "MID$", 3),
+            "RIGHT$": bo.StrOp(lambda x: x[0][-int(x[1]):], "RIGHT$", 2),
+            "RND":    bo.MonoOp(lambda x: random.random()),
+            "SGN":    bo.MonoOp(lambda x: (x > 0) - (x < 0)),
+            "SIN":    bo.MonoOp(lambda x: sin(x)),
+            "SQR":    bo.MonoOp(lambda x: sqrt(x)),  # That's Square Root, not Square. At least in this BASIC
+            "SPACE$": spaces,  # Sames as TAB function
+            "STR$":   bo.StrDollarMonoOp(lambda x: str(x), return_type="str"),
+            "TAB":    spaces,
+        }
+
