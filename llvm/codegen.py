37dbca53c234073c788bdb0d6e3cd3ed27ff5eed
cocode <github.com@worldware.com>
2025-06-22 19:43:05 -0700

    Updated to add for loop support

--- llvm/codegen.py
+++ llvm/codegen.py
@@ -24,6 +24,7 @@ class LLVMCodeGenerator:
         self.builder = None
         self.symbol_table = {}
         self.line_blocks = {}
+        self.loop_stack = []  # Track nested FOR loops
 
     def generate_ir(self):
         main_func_type = ir.FunctionType(ir.IntType(32), [])
@@ -69,6 +70,8 @@ class LLVMCodeGenerator:
             for stmt in line.stmts:
                 if isinstance(stmt, ParsedStatementLet):
                     var_names.add(stmt._variable)
+                elif isinstance(stmt, ParsedStatementFor):
+                    var_names.add(stmt._index_clause)
         
         for var_name in var_names:
             # Assuming all variables are floats for now (f64)
@@ -82,11 +85,100 @@ class LLVMCodeGenerator:
             self._codegen_print(stmt)
         elif isinstance(stmt, ParsedStatementGo):
              self._codegen_goto(stmt)
+        elif isinstance(stmt, ParsedStatementFor):
+             self._codegen_for(stmt)
+        elif isinstance(stmt, ParsedStatementNext):
+             self._codegen_next(stmt)
         elif stmt.keyword.name == "END":
              self.builder.ret(ir.Constant(ir.IntType(32), 0))
         # Add other statements here
         else:
             print(f"Warning: Codegen for statement '{type(stmt).__name__}' not implemented.")
+
+    def _codegen_for(self, stmt):
+        """Generate LLVM IR for a FOR loop"""
+        # Get the loop variable
+        loop_var = stmt._index_clause
+        var_ptr = self.symbol_table.get(loop_var)
+        if not var_ptr:
+            var_ptr = self.builder.alloca(ir.DoubleType(), name=loop_var)
+            self.symbol_table[loop_var] = var_ptr
+        
+        # Parse and evaluate start, end, and step expressions
+        lexer = get_lexer()
+        start_tokens = lexer.lex(stmt._start_clause)
+        end_tokens = lexer.lex(stmt._to_clause)
+        step_tokens = lexer.lex(stmt._step_clause)
+        
+        start_val = self._codegen_expr(start_tokens)
+        end_val = self._codegen_expr(end_tokens)
+        step_val = self._codegen_expr(step_tokens)
+        
+        # Store initial value
+        self.builder.store(start_val, var_ptr)
+        
+        # Create loop blocks
+        func = self.builder.block.function
+        cond_block = func.append_basic_block(name=f"for_{loop_var}_cond")
+        body_block = func.append_basic_block(name=f"for_{loop_var}_body")
+        after_block = func.append_basic_block(name=f"for_{loop_var}_after")
+        
+        # Branch to condition block
+        self.builder.branch(cond_block)
+        
+        # Condition block
+        self.builder.position_at_end(cond_block)
+        current_val = self.builder.load(var_ptr, name=f"load_{loop_var}")
+        
+        # Compare current value with end value
+        # For positive step: current <= end
+        # For negative step: current >= end
+        zero = ir.Constant(ir.DoubleType(), 0.0)
+        step_positive = self.builder.fcmp_ordered(">=", step_val, zero, name="step_positive")
+        
+        # Create conditional comparison
+        cmp_positive = self.builder.fcmp_ordered("<=", current_val, end_val, name="cmp_positive")
+        cmp_negative = self.builder.fcmp_ordered(">=", current_val, end_val, name="cmp_negative")
+        
+        # Select the appropriate comparison based on step sign
+        condition = self.builder.select(step_positive, cmp_positive, cmp_negative, name="loop_condition")
+        
+        # Branch based on condition
+        self.builder.cbranch(condition, body_block, after_block)
+        
+        # Push loop context onto stack
+        self.loop_stack.append({
+            'var': loop_var,
+            'var_ptr': var_ptr,
+            'step_val': step_val,
+            'cond_block': cond_block,
+            'after_block': after_block
+        })
+        
+        # Position builder at start of body block
+        self.builder.position_at_end(body_block)
+
+    def _codegen_next(self, stmt):
+        """Generate LLVM IR for a NEXT statement"""
+        if not self.loop_stack:
+            raise Exception("NEXT without corresponding FOR")
+        
+        loop_context = self.loop_stack.pop()
+        loop_var = stmt.loop_var
+        
+        if loop_var != loop_context['var']:
+            raise Exception(f"NEXT variable {loop_var} doesn't match FOR variable {loop_context['var']}")
+        
+        # Increment the loop variable
+        current_val = self.builder.load(loop_context['var_ptr'], name=f"load_{loop_var}")
+        new_val = self.builder.fadd(current_val, loop_context['step_val'], name=f"inc_{loop_var}")
+        self.builder.store(new_val, loop_context['var_ptr'])
+        
+        # Branch back to condition block
+        self.builder.branch(loop_context['cond_block'])
+        
+        # Position builder at after block for any code that follows
+        self.builder.position_at_end(loop_context['after_block'])
 
     def _codegen_goto(self, stmt):
         target_line = int(stmt.destination)
@@ -195,6 +287,7 @@ class LLVMCodeGenerator:
             return data_stack[0]
         else:
             raise Exception("Expression evaluation failed, stack has multiple values.")
+
 
 def generate_llvm_ir(program):
     codegen = LLVMCodeGenerator(program)
