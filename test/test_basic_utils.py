83e3df9e48e89287053916b0049ea50f14f264e0
cocode <github.com@worldware.com>
2025-06-23 22:32:45 -0700

    Cleanup unneeded imports

--- test/test_basic_utils.py
+++ test/test_basic_utils.py
@@ -1,15 +1,6 @@
-from io import StringIO
 from unittest import TestCase
-import sys
 
-from basic_dialect import ARRAY_OFFSET
-from basic_statements import Keywords
 from basic_utils import smart_split, format_program
-from basic_interpreter import Executor, BasicSyntaxError
-from basic_statements import is_valid_identifier
-from basic_types import SymbolType, ProgramLine, RunStatus
-from basic_loading import tokenize_line, load_program, tokenize
-
 
 class Test(TestCase):
 
