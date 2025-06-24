a99f963660be14eda6753dbbbf75e0457f734ae0
cocode <github.com@worldware.com>
2025-06-23 18:49:45 -0700

    Removed LLVM stuff (to tbc.py)

--- basic.py
+++ basic.py
@@ -4,23 +4,16 @@ Main program for running a basic program from the command line.
 import sys
 import argparse
 import pprint
-import traceback
 
 from basic_interpreter import Executor
 from basic_loading import load_program
 from basic_types import BasicSyntaxError, RunStatus
-from llvm.codegen import generate_llvm_ir
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run BASIC programs.')
     parser.add_argument('program', help="The name of the basic file to run. Will add '.bas' of not found.")
     parser.add_argument('--trace', '-t', action='store_true', help="Write  a trace of execution to 'tracefile.txt'.")
     parser.add_argument('--symbols', '-s', action='store_true', help="Dump the symbol table on exit.")
-    parser.add_argument(
-        '-l', '--llvm',
-        action='store_true',
-        help='Save the basic program as LLVM IR code to <basename>.ll'
-    )
     args = parser.parse_args()
 
     try:
@@ -32,19 +25,6 @@ if __name__ == "__main__":
     except FileNotFoundError as f:
         print(F"File not found {f}")
         sys.exit(1)
-
-
-    if args.llvm:
-        executor = Executor(program)
-        # Generate filename from input program name
-        import os
-        base_name = os.path.splitext(args.program)[0]
-        filename = f"{base_name}.ll"
-        ir_code = generate_llvm_ir(program)
-        with open(filename, "w") as f:
-            f.write(ir_code)
-        print(f"LLVM IR saved to {filename}")
-        sys.exit(0)
 
     if args.trace:
         with open("tracefile.txt", "w") as f:
